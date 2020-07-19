693cb3d8bf0c6cfef4be6d2373765ca25f05cdeb
Oliventer <AOKovalenko2@gmail.com>
2020-07-19 19:11:08 +0300

    Added close function which save member in voice time into json

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import json
 from config import token
 from discord.ext import commands
+from time_counter import BotCogs
 
 STEPAN_ID = 378318866524143627
 GUILD_ID = 545315019982897162
@@ -12,6 +13,7 @@ class Bot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix='$')
         self.load_extension('time_counter')
+        self.storage = {}
 
     def run(self):
         super().run(token, reconnect=True)
@@ -26,6 +28,12 @@ class Bot(commands.Bot):
         elif channel.id == CHANNEL_ID:
             await channel.send(f'{author.mention}, кек.')
         await self.process_commands(message)
+
+    async def close(self):
+        self.unload_extension('time_counter')
+        print(self.storage)
+        with open('total_time.json', 'w', encoding='utf-8') as f:
+            json.dump(self.storage, f, indent=4, sort_keys=True, default=str)
 
 
 if __name__ == '__main__':
